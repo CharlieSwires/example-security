1a0e949040f2c53c3bd97e3f5fca52b3234b4493
Charles Swires <charlieswires@gmail.com>
2026-05-03 01:08:20 +0100

    removed example-security

--- example-security/backend/src/main/java/com/example/security/dto/AuthResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.security.dto;
-
-import java.util.Set;
-
-public record AuthResponse(String username, Set<String> roles) {
-}
--- example-security/backend/src/main/java/com/example/security/dto/UpdateEmailRequest.java
+++
@@ -1,3 +0,0 @@
-package com.example.security.dto;
-
-public record UpdateEmailRequest(String email) {}
